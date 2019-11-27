9f542bb910a9504861f77b03e204d48efd5d6d51
firhan-fathur <firmanfatuh@gmail.com>
2019-11-27 19:52:29 +0700

    telkom

---
+++ src/component/Grid.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class Grid extends Component {
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porta leo massa, 
+                    vel bibendum ligula dignissim id. Sed non ex lacinia, rutrum ex quis, commodo leo. 
+                    Pellentesque maximus tincidunt purus, a cursus libero molestie nec. Suspendisse ultricies 
+                    mattis vulputate. Nulla consectetur scelerisque arcu, in condimentum nulla rhoncus a. Integer magna erat, ornare vitae consequat id, consectetur at felis. Duis efficitur in libero at scelerisque.
+                    </div>
+                    <div className="col-md-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porta leo massa, vel bibendum ligula dignissim id. Sed non ex lacinia, rutrum ex quis, commodo leo. Pellentesque maximus tincidunt purus, a cursus libero molestie nec. Suspendisse ultricies mattis vulputate. Nulla consectetur scelerisque arcu, in condimentum nulla rhoncus a. Integer magna erat, ornare vitae consequat id, consectetur at felis. Duis efficitur in libero at scelerisque.
+                    </div>
+                    <div className="col-md-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porta leo massa, vel bibendum ligula dignissim id. Sed non ex lacinia, rutrum ex quis, commodo leo. Pellentesque maximus tincidunt purus, a cursus libero molestie nec. Suspendisse ultricies mattis vulputate. Nulla consectetur scelerisque arcu, in condimentum nulla rhoncus a. Integer magna erat, ornare vitae consequat id, consectetur at felis. Duis efficitur in libero at scelerisque.
+                    </div> 
+                    <div className="col-md-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porta leo massa, vel bibendum ligula dignissim id. Sed non ex lacinia, rutrum ex quis, commodo leo. Pellentesque maximus tincidunt purus, a cursus libero molestie nec. Suspendisse ultricies mattis vulputate. Nulla consectetur scelerisque arcu, in condimentum nulla rhoncus a. Integer magna erat, ornare vitae consequat id, consectetur at felis. Duis efficitur in libero at scelerisque.
+                    </div> 
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default Grid;
